d90382bc52301e8594cc13a030946f53e6057a47
Fuga Kato <tresener.k@gmail.com>
2018-12-07 15:42:01 +0900

    fix indent

--- solitaire.c
+++ solitaire.c
@@ -10,12 +10,12 @@
 
 float frand()
 {
-	return (float)rand() / RAND_MAX;
+    return (float)rand() / RAND_MAX;
 }
 
 int main(int argc, char *argv[])
 {
-	GSGLOBAL *gsGlobal = gsKit_init_global();
+    GSGLOBAL *gsGlobal = gsKit_init_global();
     GSTEXTURE tex, bgTex;
 
     gsGlobal->Mode = GS_MODE_NTSC;
@@ -24,20 +24,20 @@ int main(int argc, char *argv[])
     gsGlobal->Width = 640;
     gsGlobal->Height = 448;
 
-	gsGlobal->PrimAlphaEnable = GS_SETTING_ON;
+    gsGlobal->PrimAlphaEnable = GS_SETTING_ON;
     gsGlobal->PSM = GS_PSM_CT24;
     gsGlobal->PSMZ = GS_PSMZ_16S;
 
     // デプステストをしない
     gsGlobal->ZBuffering = GS_SETTING_OFF;
-	gsKit_set_test(gsGlobal, GS_ZTEST_OFF);
+    gsKit_set_test(gsGlobal, GS_ZTEST_OFF);
 
     // DMAの何か
-	dmaKit_init(D_CTRL_RELE_OFF, D_CTRL_MFD_OFF, D_CTRL_STS_UNSPEC, D_CTRL_STD_OFF, D_CTRL_RCYC_8, 1 << DMA_CHANNEL_GIF);
-	dmaKit_chan_init(DMA_CHANNEL_GIF);
+    dmaKit_init(D_CTRL_RELE_OFF, D_CTRL_MFD_OFF, D_CTRL_STS_UNSPEC, D_CTRL_STD_OFF, D_CTRL_RCYC_8, 1 << DMA_CHANNEL_GIF);
+    dmaKit_chan_init(DMA_CHANNEL_GIF);
 
-	gsKit_init_screen(gsGlobal);
-	gsKit_mode_switch(gsGlobal, GS_ONESHOT); // glHint的なやつ
+    gsKit_init_screen(gsGlobal);
+    gsKit_mode_switch(gsGlobal, GS_ONESHOT); // glHint的なやつ
     
     u64 TexCol = GS_SETREG_RGBAQ(0x80, 0x80, 0x80, 0x80, 0x00); // 何だかわからない
 
@@ -52,8 +52,8 @@ int main(int argc, char *argv[])
     gsKit_prim_sprite_striped_texture(gsGlobal, &bgTex, 0, 0, 0, 0, gsGlobal->Width, gsGlobal->Height, bgTex.Width, bgTex.Height, 2, TexCol);
 
     float t = 0.0;
-	while(1) {
-	    //gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0x3F, 0x3F, 0x3F, 0x00, 0x00));
+    while(1) {
+        //gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0x3F, 0x3F, 0x3F, 0x00, 0x00));
         float x = tex.Width * frand();
         float dx = (1.8 + 2.0 * frand()) * (frand() < 0.5 ? -1 : 1);
         float a = 1.0;
@@ -66,10 +66,10 @@ int main(int argc, char *argv[])
                 a = max(0.0, a - 0.002);
             x += dx;
             gsKit_sync_flip(gsGlobal);
-    		gsKit_queue_exec(gsGlobal);
+            gsKit_queue_exec(gsGlobal);
         }
-	}
+    }
 
-	return 0;
+    return 0;
 }
 
